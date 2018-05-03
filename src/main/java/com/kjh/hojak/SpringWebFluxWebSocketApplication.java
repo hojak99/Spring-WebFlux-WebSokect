9cc84621997c464bcfb520e63f204160f866d5ed
KwonJH <hojak99@gmail.com>
2018-05-04 00:17:52 +0900

    Init project

---
+++ src/main/java/com/kjh/hojak/SpringWebFluxWebSocketApplication.java
@@ -0,0 +1,12 @@
+package com.kjh.hojak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFluxWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebFluxWebSocketApplication.class, args);
+	}
+}
